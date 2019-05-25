a3d7406f575791bd184a683f413fb3070cdd5955
Himel Mallick <himel.stat.iitk@gmail.com>
2019-05-25 16:32:59 -0400

    Add option for vignette

--- R/genericPackageBuilder.R
+++ R/genericPackageBuilder.R
@@ -4,12 +4,14 @@
 #' @param parentDirectory Parent directory where the package folder is located.
 #' Must be modified accordingly.
 #' @param packageName Name of the R package you are trying to build. Assumes that all necessary files are in place.
+#' @param build_vignettes Build vignette document? Default is FALSE.
 #' @export
 genericPackageBuilder<-function(packageName,
-                                parentDirectory = '/Users/Home/Dropbox (Personal)/Repos/'){
+                                parentDirectory = '/Users/Home/Dropbox (Personal)/Repos/',
+                                build_vignettes = FALSE){
   setwd(paste(parentDirectory, packageName, sep = ''))
   devtools::document()
   setwd('..')
-  devtools::install(packageName, build_vignettes = TRUE)
+  devtools::install(packageName, build_vignettes = build_vignettes)
 }
 
